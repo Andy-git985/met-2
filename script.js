2f532249067967188cc9ef169c74fa960994e716
Andy-git985 <andy.yu617@gmail.com>
2022-06-03 00:09:11 +0000

    added isHighlight

--- script.js
+++ script.js
@@ -2,33 +2,46 @@
 // https://collectionapi.metmuseum.org/public/collection/v1/objects?metadataDate=2018-10-22&departmentIds=3|9|12
 // https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=sunflowers
 // const url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects'
+// https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&q=sunflowers
+
 
 
 const searchBar = document.querySelector('#searchBar')
-let query = ''
-const url = `https://collectionapi.metmuseum.org/public/collection/v1/search?q=${query}`
 const hasImages = document.querySelector('#hasImages')
+const isHighlight = document.querySelector('#isHighlight')
 
-searchBar.addEventListener('input', (e) => {
-  console.log(e.target.value)
-  query = e.target.value
-})
 
-// const button = document.querySelector('button')
-document.addEventListener('submit',(e) => {
+let searchUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/search?'
+const objUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
+
+
+
+document.addEventListener('submit', async (e) => {
   try {
     e.preventDefault()
-    data = fetchObjects(url)
-    console.log(data)
+    
+
+    if (isHighlight.checked) {
+      searchUrl += 'isHighlight=true&'
+    } else {
+      searchUrl -= 'isHighlight=true&'
+    }
+    const query = `q=${searchBar.value}`
+    console.log(`${searchUrl}${query}`)
+    const res = await fetch(`${searchUrl}${query}`)
+    
+    const json = await res.json()
+    console.log(json) // 85
+
+    // for (let i = 0; i < json.objectIDs.length; i++) {
+    //   const res2 = await fetch(`${objUrl}${json.objectIDs[i]}`)
+    //   const json2 = await res2.json()
+    //   console.log(json2)
+    // }
+    
   }
   catch (error) {
     console.error(error)
   }
+  
 });
-
-async fetchObjects(url) {
-  const res = await fetch(url)
-  const json = await res.json()
-  return json
-}
-
