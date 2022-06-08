6e7a18fb8cdd6aa74ccf9edd36fec0dc83999f47
Andy-git985 <andy.yu617@gmail.com>
2022-06-08 22:40:33 +0000

    form branch

--- script.js
+++ script.js
@@ -1,47 +1,19 @@
-// https://collectionapi.metmuseum.org/public/collection/v1/objects?departmentIds=3|9|12
-// https://collectionapi.metmuseum.org/public/collection/v1/objects?metadataDate=2018-10-22&departmentIds=3|9|12
-// https://collectionapi.metmuseum.org/public/collection/v1/search?hasImages=true&q=sunflowers
-// const url = 'https://collectionapi.metmuseum.org/public/collection/v1/objects'
-// https://collectionapi.metmuseum.org/public/collection/v1/search?isHighlight=true&q=sunflowers
-
-
-
-const searchBar = document.querySelector('#searchBar')
-const hasImages = document.querySelector('#hasImages')
+const formElem = document.querySelector('#formElem')
 const isHighlight = document.querySelector('#isHighlight')
-
-
-let searchUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/search?'
-const objUrl = 'https://collectionapi.metmuseum.org/public/collection/v1/objects/'
-
-
-
-document.addEventListener('submit', async (e) => {
-  try {
-    e.preventDefault()
-    
-
-    if (isHighlight.checked) {
-      searchUrl += 'isHighlight=true&'
-    } else {
-      searchUrl -= 'isHighlight=true&'
-    }
-    const query = `q=${searchBar.value}`
-    console.log(`${searchUrl}${query}`)
-    const res = await fetch(`${searchUrl}${query}`)
-    
-    const json = await res.json()
-    console.log(json) // 85
-
-    // for (let i = 0; i < json.objectIDs.length; i++) {
-    //   const res2 = await fetch(`${objUrl}${json.objectIDs[i]}`)
-    //   const json2 = await res2.json()
-    //   console.log(json2)
-    // }
-    
-  }
-  catch (error) {
-    console.error(error)
-  }
   
+isHighlight.addEventListener('change', function() {
+  if (this.checked) {
+    this.setAttribute("value", "true")
+  } //else {
+  //   this.setAttribute("value", "false")  
+  // }
 });
+
+
+document.addEventListener('submit', (e) => {
+  e.preventDefault()
+  console.log(formElem)
+  for (var pair of Object.entries(formElem)) {
+    console.log(pair[0] + ': ' + pair[1].value);
+  }
+})
